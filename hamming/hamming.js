1c1de4f75414bf50f851e7fe46298eccc62b12ed
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-25 11:07:50 -0500

    changed structure, 2nd test passed

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,14 +1,26 @@
-var Hamming = function(a, b) {
-  this.val1 = a;
-  this.val2 = b;
+var Hamming = function(dna1, dna2) {
+  this.val1 = dna1;
+  this.val2 = dna2;
 };
 
-Hamming.prototype.compute = function(a, b) {
-  var length1 = a.length;
-  var length2 = b.length;
-  if (a === b) {
+Hamming.prototype.compute = function(dna1, dna2) {
+  var dnaArr1 = dna1.split("");
+  var dnaArr2 = dna2.split("");
+  if (dnaArr1[0] === dnaArr2[0]) {
+    console.log(0);
     return 0;
+  } else if (dnaArr1[0] !== dnaArr2[0]) {
+    return 1;
   }
 };
 
+var hamming = new Hamming();
+var hamming1 = new Hamming("a", "a");
+hamming.compute("a", "a");
+
 module.exports = Hamming;
+
+// compare array 1 to array 2
+// if array 1 is different from array 2, return
+// the number of differences
+// convert string to array first
