e6b54330ad6f88ad917427acb2043c9d6565c800
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-26 11:52:33 -0500

    4 tests are passing

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -4,6 +4,12 @@ var DnaTranscriber = function(dna) {
 DnaTranscriber.prototype.toRna = function(dna) {
   if (dna === "C") {
     return "G";
+  } else if (dna === "G") {
+    return "C";
+  } else if (dna === "A") {
+    return "U";
+  } else if (dna === "T") {
+    return "A";
   }
 };
 
