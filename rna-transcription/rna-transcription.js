bb30640703d0750d69d911820d5c5b402fb2f030
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-26 11:49:12 -0500

    first test passed

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,10 +1,17 @@
-var DnaTranscriber = function() {
-  //code here
+var DnaTranscriber = function(dna) {
+  this.value = dna;
 };
-DnaTranscriber.prototype.toRna = function() {
-  //code
-  //code
-  //code
+DnaTranscriber.prototype.toRna = function(dna) {
+  if (dna === "C") {
+    return "G";
+  }
 };
 
 module.exports = DnaTranscriber;
+
+// Given a DNA strand, return it's RNA complement
+// RNA DNA strands are sequ of nucleotides
+// DNA nucleotides=A C G T
+// RNA nucleotides=A C G U
+//transcribed RNA strand formed by
+// replacing each n with it's complement
