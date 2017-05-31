97255021f8f29e5c399569f6f406fe1f7e853229
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-31 07:12:20 -0500

    first test passed

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,6 +1,11 @@
-var Pangram = function(x) {
-  this.value = x;
+var Pangram = function(string) {
+  this.value = string;
 };
-Pangram.prototype.isPangram = function(x) {};
+Pangram.prototype.isPangram = function(string) {
+  return false;
+};
 
 module.exports = Pangram;
+
+// pangram is a sentence using every letter of the alphabet
+// at least once.
