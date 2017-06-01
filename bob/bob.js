4b221f941e4b75c51265809af725b3d2ddb2af0a
Andres Cifuentes <aacifuentes@gmail.com>
2017-06-01 10:23:37 -0500

    first two tests passed

---
+++ bob/bob.js
@@ -0,0 +1,16 @@
+var Bob = function(str) {
+  this.string = str;
+};
+Bob.prototype.hey = function(str) {
+  this.string = str;
+  if (str === str.toUpperCase()) {
+    return "Whoa, chill out!";
+  } else return "Whatever.";
+};
+
+module.exports = Bob;
+
+//if string is upper case, return "Whoa, chill out!"
+//if string is a question, return "Sure."
+// if string is empty, return "Fine. Be that way!"
+//if anything else, return "Whatever."
