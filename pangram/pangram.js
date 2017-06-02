2b1d6b1098e77f7ff327db9fd1af50b714b01a47
Andres Cifuentes <aacifuentes@gmail.com>
2017-06-02 14:03:02 -0500

    updated files

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,11 +1,18 @@
 var Pangram = function(string) {
-  this.value = string;
+  this.word = string;
 };
 Pangram.prototype.isPangram = function(string) {
-  return false;
+  if (this.word.length < 1) {
+    return false;
+  }
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  string = this.word.toLowerCase();
 };
 
 module.exports = Pangram;
 
 // pangram is a sentence using every letter of the alphabet
 // at least once.
+
+//go through string
+//if char appears more than once, then false
