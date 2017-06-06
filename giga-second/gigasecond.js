edf3651e83c6c5fa8aa1c6ee5432ea74b3517c1b
Andres Cifuentes <aacifuentes@gmail.com>
2017-06-06 10:25:47 -0500

    added and del

---
+++ giga-second/gigasecond.js
@@ -0,0 +1,14 @@
+var Gigasecond = function(date) {
+  this.startDate = date;
+};
+Gigasecond.prototype.date = function() {
+  var startTime = this.startDate.getTime();
+  var endDate = new Date(startTime + 1000000000000);
+  endDate.setHours(0);
+  endDate.setSeconds(0);
+  endDate.setMinutes(0);
+  return endDate;
+};
+
+module.exports = Gigasecond;
+//unfamiliar with getTime();
