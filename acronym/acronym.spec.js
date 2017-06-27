5bd4bc32e26afa55cf4ea0269f76d6eec144aec1
Andres Cifuentes <aacifuentes@gmail.com>
2017-06-27 17:04:20 -0500

    edited spec file

--- acronym/acronym.spec.js
+++ acronym/acronym.spec.js
@@ -1,28 +1,29 @@
-var Acronyms = require("./acronym");
+var Acronyms = require("./acronym.js");
 
 describe("Acronyms are produced from", function() {
+  var acronyms = new Acronyms();
   it("title cased phrases", function() {
-    expect(Acronyms.parse("Portable Network Graphics")).toEqual("PNG");
+    expect(acronyms.parse("Portable Network Graphics")).toEqual("PNG");
   });
 
   it("other title cased phrases", function() {
-    expect(Acronyms.parse("Ruby on Rails")).toEqual("ROR");
+    expect(acronyms.parse("Ruby on Rails")).toEqual("ROR");
   });
 
   it("inconsistently cased phrases", function() {
-    expect(Acronyms.parse("HyperText Markup Language")).toEqual("HTML");
+    expect(acronyms.parse("HyperText Markup Language")).toEqual("HTML");
   });
 
   it("phrases with punctuation", function() {
-    expect(Acronyms.parse("First In, First Out")).toEqual("FIFO");
+    expect(acronyms.parse("First In, First Out")).toEqual("FIFO");
   });
 
   it("other phrases with punctuation", function() {
-    expect(Acronyms.parse("PHP: Hypertext Preprocessor")).toEqual("PHP");
+    expect(acronyms.parse("PHP: Hypertext Preprocessor")).toEqual("PHP");
   });
 
   it("phrases with punctuation and sentence casing", function() {
-    expect(Acronyms.parse("Complementary metal-oxide semiconductor")).toEqual(
+    expect(acronyms.parse("Complementary metal-oxide semiconductor")).toEqual(
       "CMOS"
     );
   });
