1b19c79bb0ffdf239157a4e253fc757e3c92f23b
Andres Cifuentes <aacifuentes@gmail.com>
2017-07-12 18:18:14 -0500

    first test passed

---
+++ grade-school/grade-school.js
@@ -0,0 +1,10 @@
+var School = function(input) {
+  this.input = input;
+};
+
+School.prototype.roster = function(input) {
+  if (input === undefined) {
+    return {};
+  }
+};
+module.exports = School;
