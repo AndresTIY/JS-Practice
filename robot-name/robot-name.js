1e13a117b22f5c953c2151b87b1eb0cf472494ac
Andres Cifuentes <aacifuentes@gmail.com>
2017-07-13 16:33:28 -0500

    attempting

---
+++ robot-name/robot-name.js
@@ -0,0 +1,26 @@
+function Robot() {
+  var robotName = "CD123";
+
+  function randomName() {
+    return robotName;
+  }
+
+  return {
+    name: randomName
+  };
+}
+module.exports = Robot;
+
+//first two letters must be uppercase
+//followed by 3 digits
+
+// function School() {
+//   var roster = {};
+//
+//   function getRoster() {
+//     return roster;
+//   }
+//   return {
+//     roster: getRoster
+//   };
+// }
