aa9f3095c00e99246f7a1756cfc7f69822e696d1
Andres Cifuentes <aacifuentes@gmail.com>
2017-07-20 15:29:13 -0500

    using switch case and vars

--- food-chain/food-chain.js
+++ food-chain/food-chain.js
@@ -1,9 +1,19 @@
 function FoodChain() {
+  var endOfVerse =
+    "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+  var verse1 = "I know an old lady who swallowed a fly.\n" + endOfVerse;
+  var verse2;
   function song(num) {
-    if (num === 1) {
-      return "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n";
+    switch (num) {
+      case 1: {
+        return verse1;
+        break;
+      }
+      default:
+        return;
     }
   }
+
   return {
     verse: song
   };
