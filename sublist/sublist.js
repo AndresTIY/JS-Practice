e942b3382741524e9af23684eaa7ef3871190c44
Andres Cifuentes <aacifuentes@gmail.com>
2017-07-27 13:56:37 -0500

    second attempt

--- sublist/sublist.js
+++ sublist/sublist.js
@@ -1,6 +1,9 @@
 function Sublist(listOne) {
   this.listOne = listOne;
   function compareList(listTwo) {
+    if (Array.isArray(listTwo)) {
+      return "SUBLIST";
+    }
     if (listOne === listTwo || listOne === undefined || listTwo === undefined) {
       return "EQUAL";
     }
