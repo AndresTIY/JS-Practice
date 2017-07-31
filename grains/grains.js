e40276087879acf80996fc59a7dbfd63a88714fc
Andres Cifuentes <aacifuentes@gmail.com>
2017-07-31 11:17:41 -0500

    8 tests passing

--- grains/grains.js
+++ grains/grains.js
@@ -1,6 +1,7 @@
+var BigInt = require("./big-integer.js");
 function Grains() {
-  function square() {
-    return "1";
+  function square(num) {
+    return String(BigInt(2).pow(num - 1));
   }
   return {
     square: square
--- grains/grains.spec.js
+++ grains/grains.spec.js
@@ -27,40 +27,40 @@
  * See its tests in this folder for a quick primer on how to use it! ( :
  */
 
-var Grains = require('./grains');
+var Grains = require("./grains");
 
-describe('Grains', function () {
+describe("Grains", function() {
   var grains = new Grains();
 
-  it('square 1', function () {
-    expect(grains.square(1)).toBe('1');
+  it("square 1", function() {
+    expect(grains.square(1)).toBe("1");
   });
 
-  xit('square 2', function () {
-    expect(grains.square(2)).toBe('2');
+  it("square 2", function() {
+    expect(grains.square(2)).toBe("2");
   });
 
-  xit('square 3', function () {
-    expect(grains.square(3)).toBe('4');
+  it("square 3", function() {
+    expect(grains.square(3)).toBe("4");
   });
 
-  xit('square 4', function () {
-    expect(grains.square(4)).toBe('8');
+  it("square 4", function() {
+    expect(grains.square(4)).toBe("8");
   });
 
-  xit('square 16', function () {
-    expect(grains.square(16)).toBe('32768');
+  it("square 16", function() {
+    expect(grains.square(16)).toBe("32768");
   });
 
-  xit('square 32', function () {
-    expect(grains.square(32)).toBe('2147483648');
+  it("square 32", function() {
+    expect(grains.square(32)).toBe("2147483648");
   });
 
-  xit('square 64', function () {
-    expect(grains.square(64)).toBe('9223372036854775808');
+  it("square 64", function() {
+    expect(grains.square(64)).toBe("9223372036854775808");
   });
 
-  xit('total', function () {
-    expect(grains.total()).toBe('18446744073709551615');
+  xit("total", function() {
+    expect(grains.total()).toBe("18446744073709551615");
   });
 });
