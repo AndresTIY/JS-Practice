1b080a957efae80f143d6e6804454e184c33afe2
Andres Cifuentes <aacifuentes@gmail.com>
2017-07-31 11:19:40 -0500

    all tests are passing

--- grains/grains.js
+++ grains/grains.js
@@ -3,8 +3,12 @@ function Grains() {
   function square(num) {
     return String(BigInt(2).pow(num - 1));
   }
+  function total() {
+    return String(BigInt(2).pow(64).prev());
+  }
   return {
-    square: square
+    square: square,
+    total: total
   };
 }
 module.exports = Grains;
