4f0e05195ce4e54bfa47c8ad99cd15d9795b0270
Andres Cifuentes <aacifuentes@gmail.com>
2017-08-03 07:24:19 -0500

    4 tests passing

--- binary/binary.js
+++ binary/binary.js
@@ -1,6 +1,13 @@
 function Binary(num) {
   function toDecimal() {
-    return Number(num);
+    var l = num.length;
+    var total = 0;
+    for (var i = 0; i < l; i++) {
+      var n = l - 1;
+      var exp = num[i] * Math.pow(2, n);
+      n = n - 1;
+      return (total = total + exp);
+    }
   }
   return {
     toDecimal: toDecimal
