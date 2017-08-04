2e6213eac71259a682671238519642460321b5cd
Andres Cifuentes <aacifuentes@gmail.com>
2017-08-04 07:27:34 -0500

    setup set

---
+++ prime-factors/prime-factors.js
@@ -0,0 +1,11 @@
+function PrimeFactors() {
+  function determineIfPrime() {
+    var emptyArr = [];
+    return emptyArr;
+  }
+  return {
+    for: determineIfPrime
+  };
+}
+
+module.exports = PrimeFactors;
