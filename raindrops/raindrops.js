01968e3aa7ecfd53a6dbeafddaf7c77b66b97e81
Andres Cifuentes <aacifuentes@gmail.com>
2017-08-14 09:13:56 -0500

    all tests passing

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -2,6 +2,29 @@ var Raindrops = function(num) {
   this.num = num;
 };
 Raindrops.prototype.convert = function(num) {
-  return String(num);
+  if (num % 3 === 0 && num % 5 === 0 && num % 7 === 0) {
+    return "PlingPlangPlong";
+  }
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "PlingPlang";
+  }
+  if (num % 3 === 0 && num % 7 === 0) {
+    return "PlingPlong";
+  }
+  if (num % 5 === 0 && num % 7 === 0) {
+    return "PlangPlong";
+  }
+
+  if (num % 3 === 0) {
+    return "Pling";
+  }
+  if (num % 5 === 0) {
+    return "Plang";
+  }
+  if (num % 7 === 0) {
+    return "Plong";
+  } else {
+    return String(num);
+  }
 };
 module.exports = Raindrops;
