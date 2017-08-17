708ffb1c764dd947a0c12b6db4648c45b1709212
Andres Cifuentes <aacifuentes@gmail.com>
2017-08-17 10:13:58 -0500

    added functionality, all words beginning with vowels passing

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -1,5 +1,16 @@
-function translate() {
-  return "appleay";
+var vowels = ["a", "e", "i", "o", "u"];
+
+function translate(word) {
+  var newWord = word;
+  for (var i = 0; i < vowels.length; i++) {
+    if (word[0] === vowels[i]) {
+      newWord = newWord + "ay";
+    }
+  }
+
+  return newWord;
 }
 
 module.exports = { translate: translate };
+// rule 1: if a word begins with a vowel, add ay to end
+// rule 2: if a word begins with anything else, move it to the end
