b5aea68582c3ce6694189a456306cba5c1be2041
Andres Cifuentes <aacifuentes@gmail.com>
2017-10-02 15:55:25 -0500

    normalizePlaintext is working

--- crypto-square/crypto-square.js
+++ crypto-square/crypto-square.js
@@ -1,8 +1,22 @@
-var Crypto = function() {
+var Crypto = function(str) {
+  this.str = str;
   function normalizePlaintext() {
-    return "splunk";
+    return str
+      .toLowerCase()
+      .split("")
+      .filter(function(e) {
+        return e.match(/[a-z0-9]/);
+        // The match() method retrieves the matches when matching a string against a regular expression.
+      })
+      .join("");
   }
+
   return { normalizePlaintext: normalizePlaintext };
 };
 
 module.exports = Crypto;
+
+// normalize should remove all symbols and spaces lowercase, and then combine
+// remove symbols
+// remove spaces
+// lowercase all letters
